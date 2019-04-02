cee69214fb424125213fa220dcebf028a394a974
Renato Fabri <renato.fabri@entelgy.com>
2019-04-02 16:37:09 -0300

    More mapping and running animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,17 +7,23 @@ public class PlayerMovement : MonoBehaviour {
     public CharacterController2D controller;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
+    float horizontalMovement = 0f;
+    public Animator animator;
     bool jump = false;
 	// Update is called once per frame
 	void Update () {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Horizontal", horizontalMovement);
+        horizontalMove = horizontalMovement * runSpeed;
+        
         if (Input.GetButtonDown("Jump")) {
             jump = true;
         }
     }
 
     private void FixedUpdate() {
+        
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
 
