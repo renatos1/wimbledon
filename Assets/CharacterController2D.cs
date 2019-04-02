aa728e1d20dc7929be883ddf143b6ed0057e1568
Renato Fabri <renato.fabri@entelgy.com>
2019-04-02 18:35:26 -0300

    Animations

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -12,7 +12,7 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	const float k_GroundedRadius = .01f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
@@ -54,7 +54,7 @@ public class CharacterController2D : MonoBehaviour
 			if (colliders[i].gameObject != gameObject)
 			{
 				m_Grounded = true;
-				if (!wasGrounded)
+				if (!wasGrounded && m_Rigidbody2D.velocity.y < 0)
 					OnLandEvent.Invoke();
 			}
 		}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,23 +7,40 @@ public class PlayerMovement : MonoBehaviour {
     public CharacterController2D controller;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
-    float horizontalMovement = 0f;
     public Animator animator;
     bool jump = false;
+    bool crouch = false;
 	// Update is called once per frame
 	void Update () {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        animator.SetFloat("Horizontal", horizontalMovement);
-        horizontalMove = horizontalMovement * runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         
         if (Input.GetButtonDown("Jump")) {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+
+        /*if (Input.GetKey("left ctrl")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
+        }*/
+    }
+
+    public void OnLanding() {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching) {
+        Debug.Log("Crouching???");
+        Debug.Log(isCrouching);
+        animator.SetBool("IsCrouching", false);
     }
 
     private void FixedUpdate() {
-        
+
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
 
