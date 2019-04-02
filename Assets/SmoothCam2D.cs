b3b1690aebb638447ac6b01ce91fd02c39950345
Renato Fabri <renato.fabri@entelgy.com>
2019-04-02 14:14:20 -0300

    Add idle animation, camera follow, house collision + map changes

---
+++ Assets/SmoothCam2D.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+﻿using System.Collections;
+
+public class SmoothCam2D : MonoBehaviour {
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update() {
+        if (target) {
+            Camera camera = GetComponent<Camera>();
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z + 10));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);        
+        }
+    }
+}
