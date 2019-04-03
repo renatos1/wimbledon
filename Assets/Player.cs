cbc06c94624bf0421504954d177929ebbb4d2b4f
Fabian Cabau <fabian.cabau@entelgy.com>
2019-04-03 09:40:31 -0300

    pq diabos vc n funcionou antes

---
+++ Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isServer) {
+            SpawnRaposa();
+        }
+
+        Debug.Log("isLocalPlayer?");
+        Debug.Log(isLocalPlayer);
+        
+        if (isLocalPlayer) {
+            Camera.main.GetComponent<SmoothCam2D>().target = myRaposa.transform;
+        }   
+    }
+
+    // public override void OnStartLocalPlayer()
+    // {
+    //     Debug.Log("OnStartLocalPlayer");
+    //     Debug.Log("isLocalPlayer");
+    //     Debug.Log(isLocalPlayer);
+    //     Debug.Log("localPlayerAuthority");
+    //     Debug.Log(localPlayerAuthority);
+
+    //     if (isLocalPlayer) {
+    //         Camera.main.GetComponent<SmoothCam2D>().target = myRaposa.transform; //Fix camera on "me"
+    //     }
+
+    // }
+
+    public GameObject RaposaPrefab;
+    GameObject myRaposa;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SpawnRaposa()
+    {
+
+        // if( isServer == false )
+        // {
+        //     Debug.LogError("SpawnTank: Can only do what it's supposed to, from the SERVER!");
+        //     return;
+        // }
+
+        // This gets called by the game manager when a new round starts
+        // and a player needs a tank
+
+        // Instantiate only creates the object on the LOCAL computer. It is
+        // NOT sent to anyone else in the game
+        myRaposa = Instantiate(RaposaPrefab);
+
+        // The way to tell everyone to spawn the object in network:
+        NetworkServer.SpawnWithClientAuthority(myRaposa, connectionToClient);
+    }
+
+
+}
---
+++ Assets/Raposa.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Raposa : NetworkBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+
+    }
+
+    public override void OnStartClient()
+    {
+        Debug.Log("Raposa:OnClientConnect - isLocalPlayer");
+        Debug.Log(isLocalPlayer);
+        if (isLocalPlayer) {
+            Camera.main.GetComponent<SmoothCam2D>().target = transform;
+        } 
+    }
+
+
+    float Speed = 5;
+
+    [SyncVar]
+    Vector3 serverPosition;
+    Vector3 serverPositionSmoothVelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isServer) {
+
+        }
+
+        if (hasAuthority) {
+            AuthorityUpdate();    
+        }   
+
+        if (!hasAuthority) {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                serverPosition,
+                ref serverPositionSmoothVelocity,
+                0.25f
+            );
+        }  
+    }
+
+    void AuthorityUpdate() {
+        float movement = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+        transform.Translate(movement, 0, 0);
+        CmdUpdatePosition(transform.position);
+    }
+
+    [Command]
+    void CmdUpdatePosition(Vector3 newPosition) {
+
+        //TODO: Check to make surethis move is legal
+        // If an illegal move is spotted, do something like:
+        // RpcFixPosition( serverPosition )
+        // and return
+
+        serverPosition = newPosition;
+    }
+
+    [ClientRpc]
+    void RpcFixPosition(Vector3 newPosition) {
+        transform.position = newPosition;
+    }
+}
