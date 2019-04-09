f6bdbdd46e55402fa7fe89156265eb9df54a13d5
Fabian Cabau <fabian.cabau@entelgy.com>
2019-04-09 20:12:56 -0300

    vai tomar no cu

--- Assets/Raposa.cs
+++ Assets/Raposa.cs
@@ -5,6 +5,17 @@ using Mirror;
 public class Raposa : NetworkBehaviour
 {
 
+    private Rigidbody2D rb2d;
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Use this for initialization
+    void Awake () 
+    {
+        // anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {  
@@ -28,6 +39,10 @@ public class Raposa : NetworkBehaviour
             Camera.main.GetComponent<SmoothCam2D>().target = transform;
         }
 
+        // if (!hasAuthority) {
+        //     rb2d.isKinematic = true;
+        // }   
+
         // Turn off main camera because GamePlayer prefab has its own camera
         // GetComponentInChildren<Camera>().enabled = true;
         // Camera.main.enabled = false; 
@@ -35,10 +50,19 @@ public class Raposa : NetworkBehaviour
 
 
     float Speed = 5;
+    float jumpForce = 70;
+    [HideInInspector] public bool jump = false;
 
     [SyncVar]
     Vector3 serverPosition;
     Vector3 serverPositionSmoothVelocity;
+
+    // animate the game object from -1 to +1 and back
+    public float minimum = -1.0F;
+    public float maximum =  1.0F;
+
+    // starting value for the Lerp
+    static float t = 0.0f;
 
 
     // Update is called once per frame
@@ -48,24 +72,79 @@ public class Raposa : NetworkBehaviour
 
         }
 
+        // if (hasAuthority) {
+        //     AuthorityUpdate();    
+        // }   
+
+        // if (!hasAuthority) {
+        //     transform.position = Vector3.SmoothDamp(
+        //         transform.position,
+        //         serverPosition,
+        //         ref serverPositionSmoothVelocity,
+        //         0.25f
+        //     );
+        // }  
+    }
+
+    void FixedUpdate() 
+    {
         if (hasAuthority) {
             AuthorityUpdate();    
         }   
-
-        if (!hasAuthority) {
-            transform.position = Vector3.SmoothDamp(
-                transform.position,
-                serverPosition,
-                ref serverPositionSmoothVelocity,
-                0.25f
-            );
-        }  
     }
 
     void AuthorityUpdate() {
-        float movement = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            minimum = transform.position.y;
+            maximum = transform.position.y + 2;
+            jump = true;
+        } 
+
+        float h = Input.GetAxis("Horizontal");
+        // float _j = 0;
+        float movement = h * Speed * Time.fixedDeltaTime;
+
+
+        //    if (Input.GetButtonDown("Jump")) {
+        //     float movement = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        //    }
+        
+        // transform.Translate(movement, _j, 0);
+        // transform.position = Vector3.SmoothDamp(
+        //         transform.position,
+        //         new Vector3(movement, _j, 0),
+        //         ref velocity,
+        //         0.25f
+        //     );
+
+        // if (jump) {
+        //     transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+        //     jump = false;
+        // }
+        // rb2d.AddForce(Vector2.right * h * 100f);
         transform.Translate(movement, 0, 0);
+        
+
+        if (jump) {
+            // animate the position of the game object...
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(minimum, maximum, t), 0);
+
+            // .. and increase the t interpolater
+            t += 0.9f * Time.fixedDeltaTime;
+
+            // now check if the interpolator has reached 1.0
+            // and swap maximum and minimum so game object moves
+            // in the opposite direction.
+            if (t > 1.0f)
+            {   
+                t=0.0f;
+                jump=false;
+            }
+        }
+
+        
         CmdUpdatePosition(transform.position);
     }
 
